5340a578475591c7cbd3b5dab0b71a42e190ba24
wangyuhao@yeelight.com <wangyuhao@yeelight.com>
2021-02-19 23:19:32 +0800

    1233

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println(1);
         System.out.println("feature-b");
+        System.out.println("feature-d");
 
     }
 }
